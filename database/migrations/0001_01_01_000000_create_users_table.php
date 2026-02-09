b700f3234366e1aa82e71605faf66f1e18ddb215
IT OJT <IT OJT@tsr.local>
2026-02-09 15:22:58 +0800

    Restrict roles to only accept specified values

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
+            $table->enum('role', ['user', 'originator', 'coordinator', 'superior', 'admin']);
             $table->rememberToken();
             $table->timestamps();
         });
