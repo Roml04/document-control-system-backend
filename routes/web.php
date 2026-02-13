2877be0c6387f5c5c9cb8ca872a826b959ce28d3
Rommel <garaisrommel04@gmail.com>
2026-02-13 21:51:07 +0800

    Change landing page message

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 Route::get('/', function () {
-    return "Laravel 12";
+    return "The backend is running...";
 });
 
 Route::post('/login', function (Request $request) {
