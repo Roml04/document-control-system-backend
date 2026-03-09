adb8a855bee4eab6b690e0570256fd97b6acda52
Rommel <garaisrommel04@gmail.com>
2026-03-09 14:08:15 +0800

    Unfinished task 030926-1408

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\VersionStatus;
+use App\Enums\UserRole;
 use App\Models\Document;
-use App\Models\Revision;
-use App\Models\Version;
 use Illuminate\Http\Request;
-use Throwable;
+use Illuminate\Validation\Rules\Enum;
 
 class DocumentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $validated = $request->validate([
+          'role' => ['required', new Enum(UserRole::class)]
+        ]);
+
+        $document = Document::all();
+
+        if(!$document) {
+          return response()->json([
+            'ok' => false,
+            'data' => null,
+            'message' => 'No document available'
+          ]);
+        }
+
+        return response()->json([
+          'ok' => true,
+          'data' => $document,
+          'message' => null,
+        ]);
     }
 
     /**
--- database/migrations/2026_02_13_073447_create_documents_table.php
+++ database/migrations/2026_02_13_073447_create_documents_table.php
@@ -14,6 +14,18 @@ return new class extends Migration
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('type', ['document', 'checklist', 'form']);
+            $table->string('originator')->nullable();
+            $table->string('department')->nullable();
+            $table->string('revision_number')->nullable();
+            $table->string('revision_details')->nullable();
+            $table->date('revision_date')->nullable();
+            $table->string('approver')->nullable();
+            $table->date('approved_date')->nullable();
+            $table->string('file_path');
+            $table->string('file_name');
+            $table->foreignId('document_id')->constrained()->onDelete('cascade');
+            $table->foreignId('revision_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
