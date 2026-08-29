f0da22499fff85c8edac07b48353e0d745fccc73
Rommel <garaisrommel04@gmail.com>
2026-08-30 00:07:16 +0800

    Create file resource

---
+++ app/Http/Resources/FileResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+          "id" => $this->id,
+          "title" => $this->title,
+          "type" => $this->type,
+          "createdAt" => $this->created_at,
+          "updatedAt" => $this->updated_at,
+          
+          "latestVersion" => new VersionResource($this->version->sortByDesc('approved_date')->first()),
+          "versions" => $this->whenLoaded('version', function() {
+            return VersionResource::collection($this->version->where('status', 'published')->sortByDesc('approved_date'));
+          })
+        ];
+    }
+}
