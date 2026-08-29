7854b35cdaab9422316246bcdd8396738513d95b
Rommel <garaisrommel04@gmail.com>
2026-08-30 00:09:56 +0800

    Create request policy

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\FileResource;
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function index() {
+      $files = File::all();
+
+      return response()->json([
+        "ok" => true,
+        "data" => FileResource::collection($files),
+        "message" => "Successfully retrieved files"
+      ]);
+    }
+
+    public function view(File $file) {
+      $file->load("version");
+
+      return response()->json([
+        "ok" => true,
+        "data" => new FileResource($file),
+        "message" => "Successfully retrieved the file"
+      ]);
+    }
 }
