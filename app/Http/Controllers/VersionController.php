47487d6e28d09d64a5d6a95a1ba4f551affdf815
Rommel <garaisrommel04@gmail.com>
2026-09-02 02:03:28 +0800

    Use versionresource on view function on version controller

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\VersionResource;
 use App\Models\Request as RequestModel;
 use App\Models\Version;
 use Illuminate\Http\Request;
@@ -25,26 +26,10 @@ class VersionController extends Controller
     }
 
     public function view(Version $version) {
-      return response()->json(["ok" => true, "data" => [
-        "id" => $version->id,
-        "fileTitle" => $version->file_title,
-        "fileType" => $version->file_type,
-        "originator" => $version->originator,
-        "department" => $version->department,
-        "revisionNumber" => $version->revision_number,
-        "revisionDetails" => $version->revision_details,
-        "uploadDate" => $version->upload_date,
-        "revisionDate" => $version->revision_date,
-        "approver" => $version->approver,
-        "approvedDate" => $version->approved_date,
-        // "status" => $version->status,
-        "fileName" => $version->file_name,
-        "filePath" => $version->file_path,
-        // "fileId" => $version->file_id,
-        // "requesetId" => $version->request_id
-      ], "message" => "Retrieved version id [" . $version->id . "]"]);
 
-      // Version::where("id", )
+      return response()->json([
+        "data" => new VersionResource($version)
+      ]);
     }
 
     public function viewFile(Version $version) {
