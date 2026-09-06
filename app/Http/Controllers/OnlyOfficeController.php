7991c59016bfd56dc40f55acba502607d434f2d4
Rommel <garaisrommel04@gmail.com>
2026-09-07 00:40:27 +0800

    Check if file exists in backend and alter the key

--- app/Http/Controllers/OnlyOfficeController.php
+++ app/Http/Controllers/OnlyOfficeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\VersionResource;
 use App\Models\Version;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
@@ -14,19 +13,28 @@ class OnlyOfficeController extends Controller
     public function show(Request $request, Version $version) {
       abort_unless($request->hasValidSignature(), 403);
 
-      return Storage::response($version->file_path);
+      return Storage::response("/draft/" . $version->file_path);
     }
 
     public function edit(Version $version) {
-
       $url = URL::signedRoute('onlyoffice.document', ['version' => $version->id], absolute: true);
 
+      /**
+       * Checks if the file is existing on /draft/versions and 
+       * copies the original file and put it into /draft/versions.
+       */
+      if(Storage::missing("/draft/$version->file_path")) {
+        Storage::copy($version->file_path, "/draft/$version->file_path");
+      }
+
+      $lastModified = Storage::lastModified("/draft/$version->file_path");
+      $key = "version-$version->id-$lastModified";
+
       $config = [
-        // "documentType" => "word",
         "document" => [
           "title" => $version->file_name,
-          "fileType" => "docx",
-          "key" => $version->file_name,
+          "fileType" => pathinfo($version->file_path, PATHINFO_EXTENSION),
+          "key" => $key,
           "url" => $url,
         ],
         "editorConfig" => [
@@ -34,8 +42,10 @@ class OnlyOfficeController extends Controller
         ]
       ];
 
+      /**
+       * Creates and signs the JWT token
+       */
       $token = JWT::encode($config, config("services.onlyoffice.jwt_secret"), 'HS256');
-
       $config['token'] = $token;
       
       return response()->json([
