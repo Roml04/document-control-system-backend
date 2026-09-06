2699cbafa37d3c576a88334f8fa496328b5215b4
Rommel <garaisrommel04@gmail.com>
2026-09-07 02:25:42 +0800

    Update version details and move edited file to /versions

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -8,6 +8,7 @@ use App\Models\Version;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
@@ -35,7 +36,7 @@ class VersionController extends Controller
       ]);
     }
 
-    public function viewFile(Version $version) {
+    public function download(Version $version) {
       $filePath = $version->file_path;
 
       if(Storage::missing($filePath)) {
@@ -45,5 +46,54 @@ class VersionController extends Controller
       }
 
       return Storage::response($filePath);
+    }
+
+    public function edit(Request $request) {
+      $validated = $request->validate([
+        "id" => ["required", "exists:versions,id"],
+        "fileTitle" => ["required", "string"],
+        "fileType" => ["required", "in:document,checklist,form"],
+        "originator" => ["required", "string"],
+        "department" => ["required", "string"],
+        "revisionNumber" => ["required", "string"],
+        "revisionDetails" => ["required", "string"],
+        "approver" => ["required", "string"],
+      ]);
+
+      DB::transaction(function () use($validated) {
+        $version = Version::with('request')->findOrFail($validated["id"]);
+
+        $user = $version->request->load('user')->user;
+
+        $prevFilePath = $version->file_path;
+
+        $fileName = strtolower("$user->first_name$user->last_name") . "-" . now()->format('YmdHsu') . "." . pathinfo($version->file_path, PATHINFO_EXTENSION);
+        $filePath = "versions/" . $fileName;
+
+        $version->update([
+          "file_title" => $validated["fileTitle"],
+          "file_type" => $validated["fileType"],
+          "originator" => $validated["originator"],
+          "department" => $validated["department"],
+          "revision_number" => $validated["revisionNumber"],
+          "revision_details" => $validated["revisionDetails"],
+          "revision_date" => now(),
+          "approver" => $validated["approver"],
+          "file_name" => $fileName,
+          "file_path" => $filePath,
+        ]);
+
+        $version->request->update([
+          "status" => getNextStatus("rev", $version->request->status)
+        ]);
+
+        Storage::move("/draft/$prevFilePath", $filePath);
+      });
+
+      return response()->json([
+        "ok" => true,
+        "data" => null,
+        "message" => "Successfully patched version"
+      ]);
     }
 }
